0b83a3b821a512b7ca6bbd0ca02503a478033021
Ascensian <my.hugogerard@protonmail.com>
2024-01-22 11:19:21 +0100

    Typescript installation OK

---
+++ README.md
@@ -0,0 +1,8 @@
+## 1. FIRST SET UP TYPESCRIPT + DOCKER
+
+dotenv as a dependency
+
+## 2. STEPS TO RUN EXPRESS SERVER + TYPESCRIPT (TESTING PURPOSE ONLY)
+
+npx tsc
+node dist/index.js
---
+++ index.ts
@@ -0,0 +1,16 @@
+import { config } from 'dotenv';
+config();
+import express, { Express, Request, Response } from "express"
+
+const app: Express = express();
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("HELLO WORLD TS")
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Now listening on port ${process.env.PORT}`);
+
+})
+
+
